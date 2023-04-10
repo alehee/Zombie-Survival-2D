70e255169db694da6eebb6a3f574a6447055098f
alehee <aleksanderheese@o2.pl>
2023-04-10 21:20:29 +0200

    Finished

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject zombieStandard;
+    GameObject ZombieStandard;
 
     [SerializeField]
-    int waveDelay = 30; // seconds
+    int WaveDelay = 10; // seconds
     [SerializeField]
-    double waveZombiesMultiplyer = 1.5;
+    double WaveZombiesMultiplyer = 1.2;
+    int WaveNumber = 0;
+    int WaveLastStarted = 0;
 
-    int secondsElapsed = 0;
-    float tick = 0;
+    GameObject[] SpawnPoints;
+
+    int SecondsElapsed = 0;
+    float Tick = 0;
 
     void Start()
     {
-        
+        SpawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+        RespawnZombies();
     }
 
     void Update()
     {
-        tick += Time.deltaTime;
-        if (tick >= 1)
+        Tick += Time.deltaTime;
+        if (Tick >= 1)
         {
-            tick = 0;
-            secondsElapsed += 1;
+            Tick = 0;
+            SecondsElapsed += 1;
+            Debug.Log($"Timer elapsed: {SecondsElapsed}");
 
-            Debug.Log($"Timer elapsed: {secondsElapsed}");
+            if(WaveLastStarted + WaveDelay <= SecondsElapsed)
+            {
+                RespawnZombies();
+            }
         }
+    }
+
+    void RespawnZombies()
+    {
+        WaveNumber++;
+        WaveLastStarted = SecondsElapsed;
+
+        int zombiesCount = (int)(WaveNumber * WaveZombiesMultiplyer);
+        for (int i = 0; i < zombiesCount; i++)
+        {
+            int spawnPointNb = Random.Range(0, SpawnPoints.Length);
+            Transform zombieSpawnTransform = SpawnPoints[spawnPointNb].transform;
+            zombieSpawnTransform.position += new Vector3(i/10, i/10);
+            Instantiate(ZombieStandard, zombieSpawnTransform);
+        }
+
+        Debug.Log($"Started wave number {WaveNumber}");
     }
 }
