71d9723afde08262d3c7040b85933f3bffd85434
zgonx <marcin2000c@gmail.com>
2023-05-07 19:52:23 +0200

    +arrow repair

--- Assets/Code/Scripts/WeaponBowArrow.cs
+++ Assets/Code/Scripts/WeaponBowArrow.cs
@@ -14,7 +14,12 @@ public class WeaponBowArrow : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.TryGetComponent<Status>(out Status status))
+        if (collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (collision.gameObject.TryGetComponent<Status>(out Status status))
         {
             status.TakeDamage(Damage);
             Debug.Log($"Damage dealt to {collision.gameObject.name}: {Damage}");
