77a552aaa9bb78fc6ccbc3279e0cbbe9c717130d
zgonx <marcin2000c@gmail.com>
2023-05-18 14:29:48 +0200

    +arrow ignore wall,player

--- Assets/Code/Scripts/WeaponBowArrow.cs
+++ Assets/Code/Scripts/WeaponBowArrow.cs
@@ -17,7 +17,7 @@ public class WeaponBowArrow : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (tagsToIgnore.Contains(collision.gameObject.tag))
+        if (tagsToIgnore.Contains(collision.gameObject.tag) || collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Building"))
         {
             return;
         }
